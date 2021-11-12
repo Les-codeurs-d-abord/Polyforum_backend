d22cd4188a0f679d8335682d624d7b83eb59c057
Justin <grosjean.justin.68@gmail.com>
2021-11-12 11:53:11 +0100

    feat: modèle des utilisateurs

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,13 +1,38 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("user", {
+    email: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: Sequelize.STRING(255),
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
     firstName: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING(255)
     },
     lastName: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING(255)
     },
     age: {
-      type: Sequelize.INTEGER
+      type: Sequelize.INTEGER,
+      validate: {
+        min: 0,
+      }
+    },
+    role: {
+      type: Sequelize.ENUM('ADMIN', 'CANDIDAT', 'ENTREPRISE'),
+      allowNull: false,
+      validate: {
+        isIn: [['ADMIN', 'CANDIDAT', 'ENTREPRISE']]
+      }
     }
   });
 
