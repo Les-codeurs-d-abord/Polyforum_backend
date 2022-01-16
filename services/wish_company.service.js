6a899c4d823189f862b575ba5acd47f212f221d8
Rémi Pailharey <remipailharey@gmail.com>
2022-01-16 08:00:06 +0100

    feat: get wishes ordered by rank

--- services/wish_company.service.js
+++ services/wish_company.service.js
@@ -2,25 +2,22 @@ const db = require("../models");
 const Wish_Company = db.wish_company;
 
 exports.update = async (wishId, rank) => {
-
-    try {
-        await Wish_Company.update(
-          { rank: rank },
-          { where: { id: wishId } }
-        );
-        return rank;
-      } catch (err) {
-        throw err;
-      }
-}
+  try {
+    await Wish_Company.update({ rank: rank }, { where: { id: wishId } });
+    return rank;
+  } catch (err) {
+    throw err;
+  }
+};
 
 exports.findAllByCompanyId = async (companyId) => {
-    try {
-        const list = await Wish_Company.findAll(
-            { where: { companyId: companyId } }
-        );
-        return list;
-      } catch (err) {
-        throw err;
-      }
-}
+  try {
+    const list = await Wish_Company.findAll({
+      where: { companyId: companyId },
+      order: [[`rank`, `ASC`]],
+    });
+    return list;
+  } catch (err) {
+    throw err;
+  }
+};
