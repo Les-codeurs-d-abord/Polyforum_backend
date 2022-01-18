2655d9dd189a9ee2ccd20f08ead9f864e04f304c
Michaël BUGNONE <bugnone.michael@gmail.com>
2022-01-18 09:56:31 +0100

    CORS

--- routes/company.routes.js
+++ routes/company.routes.js
@@ -5,22 +5,22 @@ module.exports = (app) => {
   const cors = require("../middleware/cors");
 
   // Retrieve all companies
-  router.get("/", cors, companyController.companyList);
+  router.get("/", companyController.companyList);
 
   // Retrieve a single company with id
-  router.get("/:userId", cors, companyController.findById);
+  router.get("/:userId", companyController.findById);
 
   // Create a new company User
-  router.post("/", cors, companyController.createCompany);
+  router.post("/", companyController.createCompany);
 
   // Upload a profile picture
-  router.post("/:userId/uploadLogo", cors, companyController.uploadLogo);
+  router.post("/:userId/uploadLogo", companyController.uploadLogo);
 
   // Delete a single company with id
-  router.delete("/:userId", cors, companyController.deleteById);
+  router.delete("/:userId", companyController.deleteById);
 
   // Update a company profile with id
-  router.put("/:userId", cors, companyController.updateCompanyProfile);
+  router.put("/:userId", companyController.updateCompanyProfile);
 
   app.use("/api/companies", router);
 };
