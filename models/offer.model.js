8fb8d6607f410d94a13e16b43e9b137a14cc1586
Justin <grosjean.justin.68@gmail.com>
2022-01-31 11:22:20 +0100

    fix: longueur description offre

--- models/offer.model.js
+++ models/offer.model.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, Sequelize) => {
       },
     },
     description: {
-      type: Sequelize.STRING(255),
+      type: Sequelize.STRING(500),
       allowNull: false,
       validate: {
         notEmpty: true,
